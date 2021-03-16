8d58fafd3bcf6252f570846680408a0717107e55
Jochem De Schutter <jochem.de.schutter@imtek.uni-freiburg.de>
2021-03-16 10:16:53 +0100

    adjust example

--- examples/single_kite_lift_mode_simple.py
+++ examples/single_kite_lift_mode_simple.py
@@ -2,7 +2,8 @@
 
 import awebox as awe
 import matplotlib.pyplot as plt
-
+from awebox.logger.logger import Logger as awelogger
+awelogger.logger.setLevel(10)
 ########################
 # SET-UP TRIAL OPTIONS #
 ########################
@@ -12,20 +13,20 @@ options = awe.Options(True)
 
 # single kite with point-mass model
 options['user_options']['system_model']['architecture'] = {1:0}
-options['user_options']['system_model']['kite_dof'] = 3
+options['user_options']['system_model']['kite_dof'] = 6
 options['user_options']['kite_standard'] = awe.ampyx_data.data_dict()
 
 # trajectory should be a single pumping cycle with initial number of five windings
 options['user_options']['trajectory']['type'] = 'power_cycle'
-options['user_options']['trajectory']['system_type'] = 'lift_mode'
+options['user_options']['trajectory']['system_type'] = 'drag_mode'
 options['user_options']['trajectory']['lift_mode']['windings'] = 5
 
 # don't include induction effects, use simple tether drag
 options['user_options']['induction_model'] = 'not_in_use'
 options['user_options']['tether_drag_model'] = 'split'
-
-options['solver']['initialization']['groundspeed'] = 45.
-options['solver']['initialization']['inclination_deg'] = 20.
+options['model']['system_bounds']['xd']['dl_t'] = [-15.0, 15.0]
+# options['solver']['initialization']['groundspeed'] = 45.
+# options['solver']['initialization']['inclination_deg'] = 20.
 
 ##################
 # OPTIMIZE TRIAL #
@@ -35,5 +36,7 @@ options['solver']['initialization']['inclination_deg'] = 20.
 trial = awe.Trial(options, 'single_kite_lift_mode')
 trial.build()
 trial.optimize()
+import ipdb; ipdb.set_trace()
 trial.plot('level_3')
+
 plt.show()
